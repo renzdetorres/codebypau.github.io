a8e1ab6f57cae4410992dd74387b9256b4a1f7af
codebypau <paulvierdetorres19@gmail.com>
2025-08-08 12:20:33 +0800

    Update AddTodoForm.js

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -3,27 +3,42 @@ import './AddTodoForm.css'; // App styles
 
 function AddTodoForm(props) {
   const [inputText, setInputText] = useState('');
+  const [dueDate, setDueDate] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (inputText.trim() === '') {
-      alert('Please fill in all the fields before adding a TODO');
+    if ( 
+      inputText.trim() === ''||
+      dueDate.trim() === ''
+    ) {
+      alert('Please fill in all fields before adding a TODO.');
       return;
     }
-    props.onAddTodo(inputText.trim());
+    props.onAddTodo({
+      text: inputText.trim(),
+      dueDate,
+    });
     setInputText('');
+    setDueDate("");
     alert('Successfully added!');
   };
 
   return (
     <form onSubmit={handleSubmit} className="add-todo-form">
-      <input
+      <div className='input-wrapper'>
+        <input
         type="text"
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
-        placeholder="Enter a new TODO..."
-        className="todo-input"
       />
+      <label className={inputText ? "filled" : ""}>Enter a new TODO</label>
+
+      <input
+        type="date"
+        value={dueDate}
+        onChange={(e) => setDueDate(e.target.value)}
+      />
+      </div>
 
       <button type="submit" className="add-btn">
         Add TODO
